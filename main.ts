8cc8aa965081f35584032ec3baa06df29e43975e
hamza217002 <muhammadhamza9191@gmail.com>
2024-03-31 12:52:40 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.blue(" Welcome to Todos app "));
+
+let todos = []
+
+let conddition = true
+
+while(conddition)
+{
+let todoList = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your Todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more",
+            default: false
+        }
+    ]
+)
+todos.push(todoList.todo)
+conddition = todoList.addMore
+console.log(todos)
+}
